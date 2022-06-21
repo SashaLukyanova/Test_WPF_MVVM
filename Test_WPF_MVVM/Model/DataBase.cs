1de8caf0d3d484e8eb4a2b46b8bf0e46d475b1bd
MirrON88 <MirrON88@MIRRON>
2022-06-22 00:50:10 +0300

    Add project files.

---
+++ Test_WPF_MVVM/Model/DataBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_WPF_MVVM.Model
+{
+    class DataBase
+    {
+        public static IEnumerable<User> GetUsers
+        { }
+    }
+}
---
+++ Test_WPF_MVVM/Model/User.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.Mvvm;
+
+namespace Test_WPF_MVVM.Model
+{
+    public class User: ViewModelBase//INotifyPropertyChanged
+    {
+
+        public string FullName {get; set;}
+        public string Status{ get; set; }
+        public int Range { get; set; }
+        public int Steps { get; set; }
+
+
+        //private string _fullName;
+        //private string _status;
+
+        //public string FullName
+        //{
+        //    get { return _fullName; }
+        //    set
+        //    {
+        //        _fullName = value;
+        //        OnPropertyChanged("FullName");
+        //    }
+        //}
+        //public string Status
+        //{
+        //    get { return _status; }
+        //    set
+        //    {
+        //        _status = value;
+        //        OnPropertyChanged("Status");
+        //    }
+        //}        
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //public void OnPropertyChanged([CallerMemberName] string prop = "")
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        //}
+    }
+}
